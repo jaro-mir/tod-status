46bc5d7d700db329efa3d3b0b956836ccdc19f89
Jarek <j@rozanski.eu>
2012-03-12 11:11:58 +0100

    storm layout

--- project/controller/widgetController.php
+++ project/controller/widgetController.php
@@ -3,20 +3,28 @@ include_once('project/model/widgetModel.php');
 
 class WidgetController
 {
-     public $model;	
+    public $model;	
 
-     public function __construct()
-     {
-          $this->model = new WidgetModel();
-     } 
+    public function __construct()
+    {
+        $this->model = new WidgetModel();
+    } 
 
-     public function invoke()
-     {
-          $horde_nb = $this->model->getHordePlayersNb();
-          $ally_nb = $this->model->getAlliancePlayersNb();
-          $is_online = $this->model->isServerOnline();
-          $uptime = $this->model->getServerUptime();
-
-          include 'project/view/widgetView.php';
-     }
+    public function invoke()
+    {
+        $horde_nb = $this->model->getHordePlayersNb();
+        $ally_nb = $this->model->getAlliancePlayersNb();
+        $is_online = $this->model->isServerOnline();
+        $uptime = $this->model->getServerUptime();
+   
+        $layout = isset($_GET['layout'])?$_GET['layout']:null;
+        switch($layout)
+        {
+            case 'storm':
+                include 'project/view/widgetStormView.php';
+                break;
+            default:
+                include 'project/view/widgetView.php';
+        }
+    }
 }
